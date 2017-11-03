7d16f84ee4499a51bb7eec0baefb9773102dae62
proutyio <kyle@prouty.io>
2017-11-03 15:05:55 -0700

    file creation, boost library linked correctly, compiles and outputs to console

---
+++ Experimental_Code/Kyle/client.cpp
@@ -0,0 +1,24 @@
+/*
+*	Author: 	Kyle Prouty
+*
+*	Subject:	C++ socket experiments 
+*				Client
+*
+*
+*	References:
+*	http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/tutorial/tutdaytime1.html
+*
+*/
+
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+using namespace std;
+using namespace boost::asio::ip;
+
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n","Works" );
+}
