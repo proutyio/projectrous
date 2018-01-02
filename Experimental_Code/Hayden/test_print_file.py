5cfbbbb63f64e39a80fe25fdbee7969b6d88f4c5
anderrob <anderrob@oregonstate.edu>
2018-01-01 19:56:52 -0800

    fixed print_file and added testing for it.

---
+++ Experimental_Code/Hayden/test_print_file.py
@@ -0,0 +1,9 @@
+import sys
+import os
+import time
+import print_file
+
+os.system('python print_file.py test1.txt')
+os.system('python print_file.py test2.txt')
+os.system('python print_file.py test3.txt')
+os.system('python print_file.py test4.txt')
