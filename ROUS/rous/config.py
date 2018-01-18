ff00b2caec7be029d86bcb850445679917a91042
kyle <kyle@prouty.io>
2018-01-17 20:39:45 -0800

    getting the code that I lost remade. Going well. Got project structure mostly set back up. Redoing features

---
+++ ROUS/rous/config.py
@@ -0,0 +1,15 @@
+import logging as log
+import sys
+
+
+log_file = "rous/rous.log"
+
+
+def setup_logger():
+    log.basicConfig(filename=log_file,
+        format='%(asctime)s::%(levelname)s:: %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p',
+        stream=sys.stdout,
+        level=log.INFO)
+
+
--- ROUS/rous/node/node.py
+++ ROUS/rous/node/node.py
@@ -2,10 +2,9 @@ import socket
 import sys
 import logging as log
 
-import services
+from .. import config
 
 
-log_file = "rous.log"
 host = 'localhost'
 port = 5000
 server_address = (host, port)
@@ -14,45 +13,40 @@ exit = False
 
 
 
-def setup_logger():
-    log.basicConfig(filename=log_file,
-        format='%(asctime)s::%(levelname)s:: %(message)s',
-        datefmt='%m/%d/%Y %I:%M:%S %p',
-        stream=sys.stdout,
-        level=log.INFO)
 
 
 
 
+# Setup socket and bind to server address
 # Server listens for message from other nodes
 def start_server():
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        log.info("Starting server: %s ",server_address)
+        log.info("Starting server:%s ",server_address)
         sock.bind(server_address)
     except:
-        log.error("Failed to start server: %s",server_address)
+        log.error("Failed to start server:%s",server_address)
     return sock
 
 
 
 
-
-def receive_message(sock):
-    log.info("Server: %s - waiting to receive message", server_address)
+# This function will live in a while loop and waits to receive a messge
+def wait_for_message(sock):
+    log.info("Server:%s - waiting to receive message", server_address)
     try:
         data, address = sock.recvfrom(4096)
-        log.info("Server: %s - received %s from %s",server_address,(data,len(data)),address)
+        log.info("Server:%s - received %s from %s",server_address,(data,len(data)),address)
         if data:
         
             # need to enter switch statement now
             parse_message(data)
         
             sent = sock.sendto(data, address)
-            log.info('Sent return message: %s back to %s', sent, address)
-    except:
-        log.error("Server: %s - sock.recvfrom had an error",server_address)
-
+            log.info('Sent return message: %s back to %s', sent, address)   
+    except (KeyboardInterrupt,RuntimeError):
+            log.error("Server:%s - sock.recvfrom had an error",server_address)
+            exit = True
 
 
 
@@ -76,22 +70,30 @@ def run_service():
 
 
 
+def send_message():
+    pass
+
+
 
 
 def main():
-    setup_logger()
+    config.setup_logger()
     sock = start_server()
     
 
     # Main loop that waits from messages from other nodes
     while True:
         try:
-            receive_message(sock)
+            wait_for_message(sock)
+
             if not exit:
-                log.info("Shutting down server: %s",server_address) 
+                log.error("Server:%s - shutting down",server_address)
                 break
-        except:
-            log.error("Server: %s - receive message loop failure",server_address)
+
+        except(KeyboardInterrupt,RuntimeError):
+            log.error("Server:%s - main loop failure",server_address)
+            log.error("Exiting..")
+            sys.exit()
 
 
 
