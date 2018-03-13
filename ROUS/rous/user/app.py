c4c34664bc8afdcd654a6061da212dd76019339d
kyle <kyle@prouty.io>
2018-03-12 19:45:13 -0700

    moved from AJAX to socketio web sockets. React frontend can now get realtime data from the nodes. It is working right now that React emits a message every second and the server responses with the mcast listener data that is it collecting from nodes. that data instantly moves from flask server to react frontend via socketio web sockets. React is updating in real time and interacting with server that interacts with nodes.

--- ROUS/rous/user/app.py
+++ ROUS/rous/user/app.py
@@ -1,16 +1,23 @@
 from flask import Flask
+from flask_socketio import SocketIO, emit
+# from flask.ext.socketio import SocketIO, emit
 from flask import request
 from flask_cors import CORS, cross_origin
 from threading import Thread, Lock
 import sys
 import socket
 import json
-
+import signal
 import rous.utils.utils as utils
 import rous.utils.network as network
 import rous.utils.encryption as encryption
 
+from flask_socketio import SocketIO, emit
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+io = SocketIO(app)
+
 CORS(app)
 
 self_ip = network.find_my_ip()
@@ -21,20 +28,34 @@ nodes = []
 mutex = Lock()
 
 
+
+@io.on('connected')
+def connected():
+    print "%s connected" % (request.sid)
+
+
+@io.on('disconnect')
+def disconnect():
+    print "%s disconnected" % (request.sid)
+
 #
-@app.route("/listenerdata")
-def listener_data():
-	mutex.acquire()
-	try:
-		return json.dumps(data)
-	finally:
-		mutex.release()
+@io.on('change_color')
+def change_color(color):
+	print "change_color"
+	print color
+	emit('hello')
+
+
+@io.on('discover_nodes')
+def discover_nodes():
+	find_nodes()
+	emit("discover_nodes", json.dumps(nodes))
 
 
 #
 @app.route("/sendmessage")
 def send_message():
-	network.send_multicast_message("info, ",ukey,self_ip)
+	network.send_multicast_message("info, "+self_ip,ukey,self_ip)
 	return "send message"
 
 
@@ -56,6 +77,8 @@ def thread_listener(sock, address):
 	while True:
 		message, (host,port) = sock.recvfrom(1024)
 		if message:
+			if message == "stop": 
+				break
 			msg = encryption.decrypt(message, ukey)
 			mutex.acquire()
 			try:
@@ -68,23 +91,45 @@ def listener():
 	sock = network.start_multicast_receiver(self_ip)
 	t = Thread(target=thread_listener, args=(sock, self_ip))
 	t.start()
-listener()
 
 
 #
 def find_nodes():
-	network.send_multicast_message("whois, "+self_ip,ukey,self_ip)
+	network.send_multicast_message("whois",ukey,self_ip)
 	mutex.acquire()
 	try:
+		del nodes[:]
 		if data:
-			for msg in data:
-				if msg.split(",")[0] == "whois":
-					ip = msg.split(",")[1]
-					if nodes:
-						for n in nodes:
-							if ip != n:
-								nodes.append(ip)
-					else:
-						nodes.append(ip)
+			for d in data:
+				msg = d.split(',')
+				if msg[0] == "info":
+					if msg[1].strip() == "whois":
+						ip = msg[2].strip()
+						if nodes:
+							for n in nodes:
+								if (str(ip) != str(n)):
+									nodes.append(ip)
+						else:
+							nodes.append(ip)
+	except:
+		pass
 	finally:
+		del data[:]
 		mutex.release()
+
+
+#
+def handle_crtl_c(signal, frame):
+    print "\nSIGNAL: ctrl c"
+    network.send_multicast_message("stop",ukey,self_ip)
+    sys.exit(0)
+
+
+
+#	START
+###############################################
+signal.signal(signal.SIGINT, handle_crtl_c)
+# io.run(app, host='0.0.0.0', port=4242, debug=True)
+listener()
+# socketio.run(app)
+###############################################
